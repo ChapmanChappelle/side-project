49674a7a6dac2fc44a97d134ac52fa732680a1a0
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-11-11 14:38:48 -0500

    initial files and setup for react + node

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// using React from react framework 
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+  // render function dictates that what the component 
+  // will display on the screen 
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1> 
+      </div>
+    )
+  }
+
+}
+
+export default App
